6f95728aef13adb947a5192ef9a764d5eabd23c8
Hayden Szymanski <hsszyman@gmail.com>
2018-05-17 07:22:41 -0400

    finished up to exercise 1.21

---
+++ chap1/E1-5_1/E1-5_1.cc
@@ -0,0 +1,11 @@
+#include "Sales_item.h"
+#include <iostream>
+using namespace std;
+int main() {
+    Sales_item book, book2; 
+    cin >> book >> book2;
+    cout << book << endl;
+    cout << book2 << endl;
+    cout << book+book2 << endl;
+    return 0;
+}
---
+++ chap1/sec1-4/README.md
@@ -0,0 +1,1 @@
+Basic io/loops/conditionals
