fd2f39d4f41e066f00cd07db865393522f2cbd3e
Hayden Szymanski <hayden@localhost.localdomain>
2018-05-20 00:52:42 -0400

    started chapter 2, playing around with primitive types

---
+++ chap2/p2_1/built-in.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    unsigned long k = -1;
+    std::cout << k << std::endl;
+}
---
+++ chap2/p2_1/test.cpp
@@ -0,0 +1,9 @@
+#include <cstdint>
+#include <iostream>
+int main() 
+{
+    for (std::uint64_t i = 500; i>=0; --i) 
+    {
+        std::cout << "infinite!" << std::endl;
+    }
+}
