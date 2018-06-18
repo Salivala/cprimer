d8132e6e4aa4c08c8a712f72e3a5de87f41fd1e9
hsszyman <hsszyman@gmail.com>
2018-06-18 00:25:08 -0400

    finished exercise section 2.4.4

---
+++ chapter 2 variables and basic types/2.4 const qualifier/2.4.4 constexpr and constant expressions/Esec2-4_4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int exercise2_32()
+{
+    /*
+     * Exercise 2.32: Is the following code legal or not? If not, how might you make
+     * it legal?
+     *
+     * int null = 0, *p = null; :  I'm not entirely sure what the intention is here,
+     * but if you're making a variable called null, you'll need to use the & operator
+     * on the right hand assignment
+     */
+    return 0;
+}
+int main()
+{
+}
--- chapter 2 variables and basic types/2.4 const qualifier/const.cpp
+++ chapter 2 variables and basic types/2.4 const qualifier/const.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
 
-int main()
+int get_size()
+{
+    return 4;
+}
+
+int r()
 {
     const int &devils_number = 666;
     std::cout << devils_number << std::endl;
+    return 0;
+}
+
+int main()
+{
+    int null = 0, *p = &null;
 }
