42c6fcde56163d90574bcd9da10752b65f81a500
hsszyman <hsszyman@gmail.com>
2018-06-21 16:32:26 -0400

    !

---
+++ chapter 2 variables and basic types/2.6 defining our own data structures/2.6.2 using the sales_data class/Esec2-6_3/cmake-build-debug/CMakeFiles/Esec2-6_2.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Esec2-6_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapter 2 variables and basic types/2.6 defining our own data structures/2.6.2 using the sales_data class/Esec2-6_3/cmake-build-debug/CMakeFiles/Esec2-6_3.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Esec2-6_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
